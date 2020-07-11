36b2e5873c4e131d8c6227cfb21d8ad37e6b3866
Praveen Kamath <praveenkamath@Praveens-MacBook-Pro.local>
2020-07-11 19:55:59 +0530

    1st commit

---
+++ src/main/java/com/assignment/validator/MessageValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/validator/MessageValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
